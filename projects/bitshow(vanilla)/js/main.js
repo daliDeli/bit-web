7bf9e06981ff149e35714e247c1316ac4a190b98
daliDeli <bruns8900@gmail.com>
2017-12-22 08:57:23 +0100

    finishing up search

--- projects/bitshow(vanilla)/js/main.js
+++ projects/bitshow(vanilla)/js/main.js
@@ -29,11 +29,11 @@ request.done(function (response) {
 
 })
 
-$(document).on("click", "a", function(){
-    var currentID = $(this).attr("datashowid");
-    localStorage.setItem("id", currentID);
+// $(document).on("click", "a", function(){
+//     var currentID = $(this).attr("datashowid");
+//     localStorage.setItem("id", currentID);
 
-})
+// })
 
 // Search shows by name
 $(document).on("keyup", "input", function(event){
@@ -41,21 +41,39 @@ $(document).on("keyup", "input", function(event){
     console.log($(this))
     var search = $(this).val();
     console.log(`ukucano je ${search}`);
-    console.log(`sta je ${event.target.value}`)
 
     var requestShow = $.ajax({
         url: "http://api.tvmaze.com/search/shows?q="+ search,
         method: "GET"
     })
-
+    
     
     requestShow.done(function (response) {
         console.log("this is a fetch",response);
-        var ulShow = $("<ul>");
+        var createUl = $("<ul>");
+        createUl.attr("id", "showSearch")
+        createUl.text("");
         
         for(var i = 0; i < response.length; i++){
-            var liShow =  $("<li>");
+            
+            var element= response[i].show
+            console.log(element.id);
+            var createLi =  $("<li>");
+            var createAnchor = $("<a>");
+            createAnchor.attr({"class": "sendOnClick", "href": "single.html","datashowid":Number(element.id)});
+            
+
+            createLi.append(element.name);
+            createAnchor.append(createLi);
+            createUl.append(createAnchor);
         }
+
+        
+        $(".sendOnClick").click(()=>{ 
+            var searchId = $(".sendOnClick").attr("datashowid");
+            localStorage.setItem("id", searchId);
+        })
+        $(".navbar").append(createUl);
     })
 } )
     
