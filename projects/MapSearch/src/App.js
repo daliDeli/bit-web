a0e496577c1421e6ae86875559a2711cedeb6017
daliDeli <bruns8900@gmail.com>
2017-12-27 08:22:49 +0100

    minor changes

---
+++ projects/MapSearch/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MyFancyComponent from "./components/MyFancyComponent ";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MyFancyComponent/>
+      </div>
+    );
+  }
+}
+
+export default App;
